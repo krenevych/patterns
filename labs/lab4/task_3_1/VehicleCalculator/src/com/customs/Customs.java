6bfcd73e6032483e2959dd7f7f9cd3309489f5ed
Andrii Krenevych <akrenevych@gmail.com>
2022-02-14 00:50:31 +0200

    333

--- labs/lab4/task_3_1/VehicleCalculator/src/com/customs/Customs.java
+++ labs/lab4/task_3_1/VehicleCalculator/src/com/customs/Customs.java
@@ -1,6 +1,7 @@
 package com.customs;
 
+// Інтерфейс митниця
 public interface Customs {
-    float vehiclePrice(Auto auto);
-    float tax(Auto auto);
+    float vehiclePrice(Auto auto); // розрахувати ціну автомобіля
+    float tax(Auto auto);          // розрахувати варність розмитнення автомобіля
 }
