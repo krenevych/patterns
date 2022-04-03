d3a8060c28402d89bc3d3c7bca585b729ff5c53d
Andrii Krenevych <akrenevych@gmail.com>
2022-04-03 17:19:02 +0300

    Підготовка для декторатора

---
+++ Java/labs/lab6/task_2_1/src/Canvas.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Canvas extends JPanel {
+
+    private Component circle1;
+    private Component circle2;
+    private Component circle3;
+
+    public Canvas() {
+        circle1 = new Circle(60, 60, 50);
+        circle2 = new Circle(120, 60, 50);
+        circle3 = new Circle(180, 60, 50);
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        circle1.draw(graphics);
+        circle2.draw(graphics);
+        circle3.draw(graphics);
+    }
+
+}
---
+++ Java/labs/lab6/task_2_1/src/Component.java
@@ -0,0 +1,5 @@
+import java.awt.*;
+
+public interface Component {
+    void draw(Graphics graphics);
+}
