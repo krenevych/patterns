d74c8476920ce7299f54bd49205eaa830378cb8c
Andrii Krenevych <akrenevych@gmail.com>
2022-04-03 23:35:54 +0300

    підготовка задачі 6.3.2

--- Java/labs/lab6/task_3_2/src/CondimentDecorator.java
+++
@@ -1,8 +0,0 @@
-abstract public class CondimentDecorator extends Beverage {
-
-    final protected Beverage beverage;
-
-    public CondimentDecorator(Beverage beverage) {
-        this.beverage = beverage;
-    }
-}
