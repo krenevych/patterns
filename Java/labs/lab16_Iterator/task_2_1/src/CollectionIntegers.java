d212eb5a454775cb311a292b19d428019b650852
Andrii Krenevych <akrenevych@gmail.com>
2022-04-24 22:36:56 +0300

    16.2.1

---
+++ Java/labs/lab16_Iterator/task_2_1/src/CollectionIntegers.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionIntegers {
+
+    private final List<Integer> array = new ArrayList<>();
+
+    public void add(int i) {
+        array.add(i);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Iterable{" +
+                "array=" + array +
+                '}';
+    }
+
+    public int size() {
+        return array.size();
+    }
+
+    public Integer get(int i) {
+        return array.get(i);
+    }
+
+
+
+}
---
+++ Java/labs/lab16_Iterator/task_2_1/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CollectionIntegers integers = new CollectionIntegers();
+        integers.add(110);
+        integers.add(312);
+        integers.add(111);
+        integers.add(813);
+        integers.add(131);
+        integers.add(140);
+
+//        for (Integer integer: integers) {
+//            System.out.println(integer);
+//        }
+
+    }
+
+}
