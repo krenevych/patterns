0b8b7f43cd2774e9b1476f3ad466701884454da2
Andrii Krenevych <akrenevych@gmail.com>
2022-05-18 11:31:06 +0300

    1211

---
+++ Python/labs/lab23_Visitor/task1.py
@@ -0,0 +1,26 @@
+class GeneralStaff():
+    def __init__(self, generals, secretPaper):
+        self.generals = generals
+        self.secretPaper = secretPaper
+
+    def __str__(self):
+        return f"GeneralStaff: У генеральному штабі є {self.generals} геренералів та {self.secretPaper} секретних паперів."
+
+
+class MilitaryBase():
+    def __init__(self, officers, soldiers, jeeps, tanks):
+        self.officers = officers
+        self.soldiers = soldiers
+        self.jeeps = jeeps
+        self.tanks = tanks
+
+    def __str__(self):
+        return f"MilitaryBase: На військовій базі є {self.officers} офіцерів, {self.soldiers} солдатів, {self.jeeps} джипів та {self.tanks} танків."
+
+
+if __name__ == '__main__':
+    generalStaff = GeneralStaff(20, 100)
+    print(generalStaff)
+
+    militaryBase = MilitaryBase(10, 1000, 300, 20)
+    print(militaryBase)
