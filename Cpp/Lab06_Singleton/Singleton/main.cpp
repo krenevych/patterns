913d332e555a2ac81d1fc5de4dfb258d19ebb721
Andrii Krenevych <krenevych@knu.ua>
2022-11-03 21:25:33 +0200

    Update main.cpp

--- Cpp/Lab06_Singleton/Singleton/main.cpp
+++ Cpp/Lab06_Singleton/Singleton/main.cpp
@@ -11,8 +11,10 @@ public:
         return *instance;
     }
 
-    void log(const string& message){
-        cout << message << "\n";
+    void log(const string& tag, const string& message){
+        fprintf(mStream, "[%s]: %s\n", tag.c_str(), message.c_str());
+        fflush(mStream);
+//        cout << message << "\n";
     }
 
     Logger(const Logger &) = delete;
@@ -21,22 +23,32 @@ public:
 private:
 
     Logger() {
-        cout << "Created instance\n";
+//        cout << "Created instance\n";
+        mStream = fopen("log.txt", "w");
+
+        atexit([](){
+            delete instance;
+        });
     }
 
     virtual ~Logger() {
-        cout << "Destroed\n";
+//        cout << "Destroed\n";
+        fclose(mStream);
     }
 
     static Logger* instance;
+
+    FILE *mStream;
 };
 
 Logger* Logger::instance;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    std::cout << "Hello, World!" << std::endl;
 
-    Logger::getInstance().log("Hello");
+    Logger::getInstance().log("Singleton", "Hello");
+    Logger::getInstance().log("Singleton", "World");
+    Logger::getInstance().log("Singleton", "!");
 
     return 0;
 }
