ef710ef0fbf107b7d5a54df134c5952b144d2626
Artem <artem.tsvik1999@gmail.com>
2023-01-06 21:39:45 +0200

    refactor cpp task 10.2.2

--- Cpp/lab10_Decorator/task_2_2/src/pizzas.cpp
+++ Cpp/lab10_Decorator/task_2_2/src/pizzas.cpp
@@ -6,7 +6,7 @@ Pizza::Pizza() {}
 
 Pizza::~Pizza() {}
 
-std::string Pizza::toString()
+std::string Pizza::toString() const
 {
   return "Pizza: " + this->description() + ", $" + std::to_string(this->cost());
 }
@@ -17,12 +17,12 @@ ThickCrustPizza::ThickCrustPizza() {}
 
 ThickCrustPizza::~ThickCrustPizza() {}
 
-std::string ThickCrustPizza::description()
+std::string ThickCrustPizza::description() const
 {
   return "ThickCrustPizza";
 }
 
-float ThickCrustPizza::cost()
+float ThickCrustPizza::cost() const
 {
   return 2.5f;
 }
@@ -33,12 +33,12 @@ ThinCrustPizza::ThinCrustPizza() {}
 
 ThinCrustPizza::~ThinCrustPizza() {}
 
-std::string ThinCrustPizza::description()
+std::string ThinCrustPizza::description() const
 {
   return "ThinCrustPizza";
 }
 
-float ThinCrustPizza::cost()
+float ThinCrustPizza::cost() const
 {
   return 2.0f;
 }
