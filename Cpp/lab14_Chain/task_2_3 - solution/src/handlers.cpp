d1d9abade11202724ffddff75092c0bd9a21383d
Artem <artem.tsvik1999@gmail.com>
2023-01-17 17:28:41 +0200

    cpp solution 14.2.3

---
+++ Cpp/lab14_Chain/task_2_3 - solution/src/handlers.cpp
@@ -0,0 +1,85 @@
+#include "handlers.hpp"
+
+#include "boost/date_time/gregorian/gregorian.hpp"
+#include "boost/date_time/posix_time/posix_time.hpp"
+
+
+//PropertyHandler
+PropertyHandler::PropertyHandler() {}
+
+PropertyHandler::~PropertyHandler() {}
+
+void PropertyHandler::setProperty(Image& image)
+{
+  if (this->next != nullptr)
+  {
+    this->next->setProperty(image);
+  }
+}
+
+void PropertyHandler::setNext(Handler* handler)
+{
+  this->next = handler;
+}
+
+//PropertyHandlerCamera
+PropertyHandlerCamera::PropertyHandlerCamera(std::string const& cameraName)
+: cameraName(cameraName) {}
+
+PropertyHandlerCamera::~PropertyHandlerCamera() {}
+
+void PropertyHandlerCamera::setProperty(Image& image)
+{
+  image.setCamera(this->cameraName);
+  this->PropertyHandler::setProperty(image);
+}
+
+//PropertyHandlerDateTime
+PropertyHandlerDateTime::PropertyHandlerDateTime() {}
+
+PropertyHandlerDateTime::~PropertyHandlerDateTime() {}
+
+void PropertyHandlerDateTime::setProperty(Image& image)
+{
+  boost::posix_time::ptime timeLocal = boost::posix_time::second_clock::local_time();
+
+  boost::posix_time::time_duration durObj = timeLocal.time_of_day();
+  std::string time = boost::posix_time::to_simple_string(durObj);
+  image.setCreationTime(time);
+
+  boost::gregorian::date dateObj = timeLocal.date();
+  std::string date = boost::gregorian::to_iso_extended_string(dateObj);
+  image.setCreationDate(date);
+
+  this->PropertyHandler::setProperty(image);
+}
+
+//PropertyHandlerName
+PropertyHandlerName::PropertyHandlerName() {}
+
+PropertyHandlerName::~PropertyHandlerName() {}
+
+void PropertyHandlerName::setProperty(Image& image)
+{
+  std::string creationDate = image.getCreationDate();
+  std::string creationTime = image.getCreationTime();
+  std::string type = image.getTypeString();
+
+  std::string name = "IMG_" + creationDate + "_" + creationTime + "." + type;
+  image.setName(name);
+
+  this->PropertyHandler::setProperty(image);
+}
+
+//PropertyHandlerType
+PropertyHandlerType::PropertyHandlerType(Image::Type const type)
+: type(type) {}
+
+PropertyHandlerType::~PropertyHandlerType() {}
+
+void PropertyHandlerType::setProperty(Image& image)
+{
+  image.setType(type);
+
+  this->PropertyHandler::setProperty(image);
+}
---
+++ Cpp/lab14_Chain/task_2_3 - solution/src/image.cpp
@@ -0,0 +1,94 @@
+#include "image.hpp"
+
+
+Image::Image() {}
+
+Image::~Image() {}
+
+Image::Type Image::getType() const
+{
+  return this->type;
+}
+
+std::string Image::getTypeString() const
+{
+  std::string type_str;
+
+  switch (this->type)
+  {
+    case Image::Type::bmp:
+    {
+      type_str = "bmp";
+      break;
+    }
+    case Image::Type::png:
+    {
+      type_str = "png";
+      break;
+    }
+    case Image::Type::jpg:
+    {
+      type_str = "jpg";
+      break;
+    }
+  }
+
+  return type_str;
+}
+
+void Image::setType(Image::Type type)
+{
+  this->type = type;
+}
+
+std::string Image::getCamera() const
+{
+  return this->camera;
+}
+
+void Image::setCamera(std::string camera)
+{
+  this->camera = camera;
+}
+
+std::string Image::getName() const
+{
+  return this->name;
+}
+
+void Image::setName(std::string name)
+{
+  this->name = name;
+}
+
+std::string Image::getCreationDate() const
+{
+  return this->creationDate;
+}
+
+void Image::setCreationDate(std::string creationDate)
+{
+  this->creationDate = creationDate;
+}
+
+std::string Image::getCreationTime() const
+{
+  return this->creationTime;
+}
+
+void Image::setCreationTime(std::string creationTime)
+{
+  this->creationTime = creationTime;
+}
+
+std::ostream& operator<<(std::ostream& os, Image const& image)
+{
+  os << "Image{type=" << image.getTypeString() <<
+        ", camera=" << image.camera <<
+        ", creationDate=" << image.creationDate <<
+        ", creationTime=" << image.creationTime <<
+        ", name=" << image.name <<
+        "}";
+
+  return os;
+}
