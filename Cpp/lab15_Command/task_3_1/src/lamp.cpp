e9069e97dd1a558ad842b1929cab473f4a49376b
Artem <artem.tsvik1999@gmail.com>
2023-01-18 08:51:55 +0200

    cpp task 15.3.1

---
+++ Cpp/lab15_Command/task_3_1/src/lamp.cpp
@@ -0,0 +1,34 @@
+#include "lamp.hpp"
+
+#include <iostream>
+
+
+Lamp::Lamp(std::string const& name)
+: name(name), isLightOn(false) {}
+
+Lamp::Lamp()
+: Lamp("default") {}
+
+Lamp::~Lamp() {}
+
+void Lamp::lightOn()
+{
+  if (this->isLightOn)
+  {
+    return;
+  }
+
+  std::cout << name << ": Light is on" << std::endl;
+  this->isLightOn = true;
+}
+
+void Lamp::lightOff()
+{
+  if (!(this->isLightOn))
+  {
+    return;
+  }
+
+  std::cout << name << ": Light is off" << std::endl;
+  this->isLightOn = false;
+}
